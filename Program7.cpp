160d7a3dd6b832f7eba19577dd8e4c0f9905115d
mattf7171 <mattf7171@gmail.com>
2024-10-28 19:07:30 -0600

    update

--- Program7.cpp
+++ Program7.cpp
@@ -4,6 +4,24 @@
 #include <iostream>
 using namespace std;
 
+int permWithRepeats(int n, int r) {
+    int result = n;
+    for (int i = 1; i < r; i++) {
+        result = result * n;
+    }
+    return result;
+}
+
+int factorial(int num) {
+    int result = num;
+    int fact = num - 1;
+    for (int i = 0; i < num - 2; i++) {
+        result = result * fact;
+        fact = fact - 1;
+    }
+    return result;
+}
+
 int main() {
 
     int n, r;
@@ -12,11 +30,15 @@ int main() {
     cout << endl;
     cout << "Please Enter Value for r: ";
     cin >> r;
-    cout << endl;
+    int enMinusAr = n - r;
 
+    int permWithRep = permWithRepeats(n, r);
+    cout << endl << "P(n,r) with repeats: " << permWithRep << endl;
 
-
+    int permWithoutRep = factorial(n) / factorial(enMinusAr);
+    cout << "P(n,r) without repeats: " << permWithoutRep << endl;
 
 
     return 0;
 }
+
