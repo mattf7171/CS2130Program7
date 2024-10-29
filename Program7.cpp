38ec549ac9dd2b3d52f99c3d6caeadf4b6319883
mattf7171 <mattf7171@gmail.com>
2024-10-28 18:38:19 -0600

    update1

---
+++ Program7.cpp
@@ -0,0 +1,22 @@
+// CS 2130 Comp. Struct.
+// Program 7
+// Matthew Feil
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, r;
+    cout << "Please Enter Value for n: ";
+    cin >> n;
+    cout << endl;
+    cout << "Please Enter Value for r: ";
+    cin >> r;
+    cout << endl;
+
+
+
+
+
+    return 0;
+}
